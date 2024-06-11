6fa67495431ee14bb078159824af13a43c9d9664
ontheway-01 <eunhwa6087@naver.com>
2024-06-11 21:31:01 +0900

    add make directory code

--- test_voice_trans.py
+++ test_voice_trans.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 import shutil
 import mimetypes
+# from openai import OpenAI
+import time
 
 app = FastAPI()
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -15,7 +17,6 @@ print(TTS().list_models())
 
 # Init TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-
 class TextToSpeechRequest(BaseModel):
     voice: str
     language: str
@@ -57,6 +58,37 @@ def find_file_by_name(file_name: str) -> str:
         return "no_file_found"
     print(matching_files[0])
     return matching_files[0]
+#
+# @app.post("/tts-openai/")
+# async def TtsAiVoice(request: TextToSpeechRequest):
+#     text = request.text
+#     language = request.language
+#     voice = request.voice
+#
+#     check_file_path = f"my/output/{voice}_{language}.mp3"
+#     if os.path.exists(check_file_path):
+#         os.remove(check_file_path)
+#
+#     os.makedirs(os.path.dirname("my/output/"), exist_ok=True)
+#     file_path = os.path.join("my/output/", f"{voice}_{language}.mp3")
+#
+#     speaker_audio_path = find_file_by_name(voice)
+#     print(speaker_audio_path)
+#     print(language)
+#
+#     response = client.audio.speech.create(
+#         model="tts-1",
+#         voice="alloy",
+#         input=text
+#     )
+#
+#     response.stream_to_file(file_path)
+#
+#     with open(file_path, 'rb') as file:
+#         audio_data = file.read()
+#
+#     return Response(content=audio_data, media_type='audio/mpeg')
+
 
 @app.post("/tts/")
 async def TtsAiVoice(request: TextToSpeechRequest):
@@ -64,17 +96,34 @@ async def TtsAiVoice(request: TextToSpeechRequest):
     language = request.language
     voice = request.voice
 
+    start_time = time.time()
     file_path = f"my/output/{voice}_{language}.mp3"
     if os.path.exists(file_path):
         os.remove(file_path)
 
+    os.makedirs(os.path.dirname("my/output/"), exist_ok=True)
+    file_path = os.path.join("my/output/", f"{voice}_{language}.mp3")
+
+    end_time = time.time()
+    elasped_time = end_time - start_time
+    print(f"make output file path : {elasped_time}")
+
+    start_time = time.time()
     speaker_audio_path = find_file_by_name(voice)
     print(speaker_audio_path)
     print(language)
+    end_time = time.time()
+    elasped_time = end_time - start_time
+    print(f"find voice file path : {elasped_time}")
 
+    start_time = time.time()
     tts.tts_to_file(text=text, speaker_wav=speaker_audio_path, language=language,file_path=file_path)
     with open(file_path, 'rb') as file:
         audio_data = file.read()
+
+    end_time = time.time()
+    elasped_time = end_time - start_time
+    print(f"generate tts audio data : {elasped_time}")
 
     print(file.content_type)
     return Response(content=audio_data, media_type='audio/mpeg')
